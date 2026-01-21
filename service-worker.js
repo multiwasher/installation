ac723abae1303f7d6a847fa0bde11909d8c117c0
multiwasher <mariofragoso82@gmail.com>
2026-01-21 15:05:52 +0000

    Auto-save changes

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker para Somengil Compliance Portal
 // Versão 5.0 - Caching e funcionamento offline
 
-const CACHE_NAME = 'somengil-compliance-v11';
+const CACHE_NAME = 'somengil-compliance-v10';
 const STATIC_ASSETS = [
   './',
   './index.html',
